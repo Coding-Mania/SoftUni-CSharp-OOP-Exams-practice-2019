bf62784e1f8f83f9dbfed1752e73b256cfd7a4b5
CodingMania <vasilmitovv@gmail.com>
2020-01-18 10:36:35 +0200

    Code refactoring

---
+++ C# OOP Advanced Exam Retake - 7 September 2017/Structure_Skeleton/Structure_Skeleton/Core/Commands/DayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DayCommand : Command
+{
+    private readonly IHarvesterController harvesterController;
+    private readonly IProviderController providerController;
+
+    public DayCommand(
+        IList<string> arguments,
+        IHarvesterController harvesterController,
+        IProviderController providerController)
+        : base(arguments)
+    {
+        this.harvesterController = harvesterController;
+        this.providerController = providerController;
+    }
+
+    public override string Execute()
+    {
+        var result = this.providerController.Produce() + "\n" + this.harvesterController.Produce();
+
+        return result;
+    }
+}
